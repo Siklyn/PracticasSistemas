f7c9b17432bfae8850086e07cfc7fa9ddac13908
Siklyn <angallade@gmail.com>
2024-06-23 23:34:16 -0600

    Calccomplete
    Se finalizo la calculadora

--- Calculadora/Assets/Scripts/Calculadora.cs
+++ Calculadora/Assets/Scripts/Calculadora.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Calculadora : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField primerNum;
+    public TMP_InputField segundoNum;
+    public TMP_Text resultadotext;
+    public int resultado;
+
+    public void Suma()
     {
-        
+        int numero1 = int.Parse(primerNum.text);
+        int numero2 = int.Parse(segundoNum.text);
+
+        resultado = numero1 + numero2;
+        resultadotext.text = resultado.ToString();
+    }
+    public void Resta()
+    {
+        int numero1 = int.Parse(primerNum.text);
+        int numero2 = int.Parse(segundoNum.text);
+
+        resultado = numero1 - numero2;
+        resultadotext.text = resultado.ToString();
+    }
+    public void Multiplicacion()
+    {
+        int numero1 = int.Parse(primerNum.text);
+        int numero2 = int.Parse(segundoNum.text);
+
+        resultado = numero1 * numero2;
+        resultadotext.text = resultado.ToString();
+    }
+    public void Division()
+    {
+        int numero1 = int.Parse(primerNum.text);
+        int numero2 = int.Parse(segundoNum.text);
+
+        resultado = numero1 / numero2;
+        resultadotext.text = resultado.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
